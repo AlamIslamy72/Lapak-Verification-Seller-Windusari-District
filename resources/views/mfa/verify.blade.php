ffbcb126f7e2d755003e506ef7a9e7f1741f5710
AlifAl4m72 <hambaami@gmail.com>
2026-08-04 14:02:59 +0700

    Tambah tombol tampilkan QR code di halaman verifikasi MFA

--- resources/views/mfa/verify.blade.php
+++ resources/views/mfa/verify.blade.php
@@ -16,7 +16,12 @@
             name="one_time_password" maxlength="6" required autofocus />
         <x-input-error :messages="$errors->get('one_time_password')" class="mt-2" />
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="flex items-center justify-between mt-4">
+            <a href="{{ route('mfa.setup') }}"
+                class="text-sm text-gray-600 underline hover:text-gray-900">
+                Tampilkan QR Code
+            </a>
+
             <x-primary-button>
                 Verifikasi
             </x-primary-button>
